db91f9ceb23118e03472e6ab882bf0d4e0df2f7e
Danielalab <olabolacrayola@hotmail.com>
2018-12-20 15:31:08 -0500

    adding snackbar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import { addNote } from "./firebase.js";
 
 (() => {
+  // seleccionando los elementos del DOM
   const input = document.getElementById('input-new-note');
   const buttonAddNote = document.getElementById('btn-add-note');
   const containerNotesList = document.getElementById('notes-list');
+  const snackbarContainer = document.getElementById('demo-snackbar');
 
   buttonAddNote.addEventListener('click', event => {
     event.preventDefault();
+    const data = {
+      message: '',
+      timeout: 2000,
+      actionText: 'Undo'
+    };
     addNote(input.value)
       .then(() => {
-        
-      }).catch((err) => {
-        
+        input.value = '';
+        data.message = 'Nota agregada'
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+      }).catch(() => {
+        input.value = '';
+        data.message = 'Lo sentimos, no se pudo agregar la nota';
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
       });
-    input.value = '';
   });
-  
+
 })();
