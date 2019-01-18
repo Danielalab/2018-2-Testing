187a0cbe3d86d879bd419e187ae39b50a69dc197
Danielalab <olabolacrayola@hotmail.com>
2019-01-18 12:25:21 -0500

    update view control

--- src/index.js
+++ src/index.js
@@ -1,64 +1,79 @@
 import { addNote, getNotes, deleteNote } from "./firebase.js";
 
 (() => {
+  const root = document.querySelector('#root');
+
   // seleccionando los elementos del DOM
-  const input = document.getElementById('input-new-note');
-  const buttonAddNote = document.getElementById('btn-add-note');
-  const containerNotesList = document.getElementById('notes-list');
-  const snackbarContainer = document.getElementById('demo-snackbar');
+  const showHome = () => {
+    root.innerHTML = '';
+    root.innerHTML = Home();
+    const input = document.getElementById('input-new-note');
+    const buttonAddNote = document.getElementById('btn-add-note');
+    const containerNotesList = document.getElementById('notes-list');
+    const snackbarContainer = document.getElementById('demo-snackbar');
 
-  buttonAddNote.addEventListener('click', (event) => {
-    event.preventDefault();
-    // data que muestra el snackbar
-    const data = {
-      message: '',
-      timeout: 2000,
-      actionText: 'Undo'
-    };
+    buttonAddNote.addEventListener('click', (event) => {
+      event.preventDefault();
+      // data que muestra el snackbar
+      const data = {
+        message: '',
+        timeout: 2000,
+        actionText: 'Undo'
+      };
 
-    addNote(input.value)
-      .then(() => {
-        input.value = '';
-        data.message = 'Nota agregada'
-        snackbarContainer.MaterialSnackbar.showSnackbar(data);
-      }).catch(() => {
-        input.value = '';
-        data.message = 'Lo sentimos, no se pudo agregar la nota';
-        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+      addNote(input.value)
+        .then(() => {
+          input.value = '';
+          data.message = 'Nota agregada'
+          snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        }).catch(() => {
+          input.value = '';
+          data.message = 'Lo sentimos, no se pudo agregar la nota';
+          snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        });
+    });
+
+    const showNotes = (notes) => {
+      containerNotesList.innerHTML = '';
+      notes.forEach((objNote) => {
+        const divElement = document.createElement('div');
+        divElement.classList.add('mdl-list__item');
+        divElement.innerHTML = `
+          <span class="mdl-list__item-primary-content">
+            <span>${objNote.title}</span>
+          </span>
+          <a class="mdl-list__item-secondary-action" id="btn-deleted-${objNote.id}" href="#">
+            <i class="material-icons">delete</i>
+          </a>
+        `;
+        containerNotesList.appendChild(divElement);
+
+        // agregando evento de click al btn eliminar una nota
+        document.querySelector(`#btn-deleted-${objNote.id}`)
+          .addEventListener('click', () => {
+            deleteNote(objNote.id);
+          });
       });
-  });
+    }
 
-  const addEventsToDeleteButtons = () => {
-    const btnsDelete = document.querySelectorAll('.btn-delete');
-    [...btnsDelete].forEach((btn) => {
-      btn.addEventListener('click', () => {
-        const id = btn.dataset.id;
-        deleteNote(id);
-      })
+    getNotes(showNotes);
+  }
+
+  const showSignIn = () => {
+    root.innerHTML = '';
+    root.innerHTML = Login();
+
+    // selecccionando elementos del DOM
+    const btnSignIn = document.querySelector('#btn-sign-in');
+
+    btnSignIn.addEventListener('click', () => {
+      const email = document.querySelector('#email').value;
+      const password = document.querySelector('#password').value;
+      signIn(email, password)
+        .then(() =>  showHome())
+        .catch(() => showSignIn())
     })
   }
 
-  const showNotes = (notes) => {
-    let templateList = '';
-    notes.forEach((objNote) => {
-      templateList +=
-        `
-        <li class="mdl-list__item">
-          <span class="mdl-list__item-primary-content">
-            ${objNote.title}
-          </span>
-          <span class="mdl-list__item-secondary-action">
-            <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored btn-delete" data-id="${objNote.id}">
-              <i class="material-icons">delete_outline</i>
-            </button>
-          </span>
-        </li>
-        `;
-    });
-    containerNotesList.innerHTML = templateList;
-    addEventsToDeleteButtons();
-  }
-
-  getNotes(showNotes);
-
+  showSignIn();
 })();
